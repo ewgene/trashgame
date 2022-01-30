f3a8a55331e6684cbfb58db0e1cee8c2632beb0a
JumpimgJack <ewgene.shure@gmail.com>
2022-01-30 08:10:11 +0700

    background built

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,14 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
+/*  rules: [
+    {
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }
+  ]*/
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,155 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="top">
+      <h1>Выбери правильный бак<br />для сортировки мусора</h1>
+      <div class="base-timer">
+        <svg class="base-timer__svg" 
+          viewBox="0 0 214 214" 
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            class="base-timer__path-elapsed"
+            d="
+              M 107, 107
+              m -100, 0
+              a 100,100 0 1,0 200,0
+              a 100,100 0 1,0 -200,0
+            "
+          ></path>
+        </svg>
+      </div>
+    </div>
+    <div class="bins">
+      <div class="bin"
+        v-for="bin in wasteTypes"
+          :id="bin.type"
+          :key="bin.type">
+          <img :src="require('@/assets/' + bin.background)">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      wasteTypes: [
+        {
+          type: "recyclable",
+          name:"Вторичные",
+          color: "#ffab00",
+          position: null,
+          background: "wastebox_yellow.png"
+        },
+        {
+          type: "mixed",
+          name: "Смешанные",
+          color: "#6ac000",          
+          position: null,
+          background: "wastebox_green.png"
+        },
+        {
+          type: "domestic",
+          name: "Бытовые",
+          color: "#6ac000",
+          position: null,
+          background: "wastebox_blue.png"
+        },
+        {
+          type: "dangerous",
+          name: "Опасные",
+          color: "#ff2020",
+          position: null,
+          background: "wastebox_gray.png"
+        }
+      ]
+    }
+  },
+  methods: {
+    getBinPositions() {
+      let bins = document.querySelector('.bins').children
+      for(let i = 0; i<this.wasteTypes.length; i++) {
+        this.wasteTypes[i].position = bins[i].getBoundingClientRect().x
+      }
+    }
+  },
+  mounted() {
+    this.getBinPositions()
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss">
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.hello {
+  width: 66vw;
+  height: 85vh;
+  margin: 0 auto;
+  background: #fff;
+  border-radius: 30px;
+  position: relative;
+  overflow: hidden;
+
+  .top {
+    width: 100%;
+  }
+
+  .bins {
+    width: 640px;
+    margin: 110px auto;
+    position: relative;
+    text-align: justify;
+  }
 }
-li {
+.bin {
+  width: 150px;
+  height: 208px;
   display: inline-block;
-  margin: 0 10px;
+  margin: 0 5px;
+
+  img {
+    width: 100%;
+  }
 }
-a {
-  color: #42b983;
+.base-timer {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+
+  &__svg {
+    transform: scaleX(-1);
+    fill: #fff!important;
+    stroke: #af63e6;
+    stroke-width: 12px;
+    stroke-linecap: round;
+  }
+  
+  @keyframes dash {
+    from {
+      stroke-dashoffset: 628;
+    }
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+  
+  &__path-elapsed {
+    stroke-dasharray: 628;
+    animation: dash 60s linear forwards;
+  }
+
+  &__path-remaining {
+    stroke-width: 7px;
+    stroke-linecap: round;
+    transform: rotate(90deg);
+    transform-origin: center;
+    transition: 1s linear all;
+    fill-rule: nonzero;
+    stroke: currentColor;
+  }
 }
 </style>
